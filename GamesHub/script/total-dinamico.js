5dc1bf226eba86036b5ed24429d0a724f6cb36a5
avrO6 <127057199+avrO6@users.noreply.github.com>
2024-02-05 18:51:33 +0100

    Update total-dinamico.js

--- GamesHub/script/total-dinamico.js
+++ GamesHub/script/total-dinamico.js
@@ -2,7 +2,7 @@
 let boton = document.getElementsByName('descuento');
 let span = document.getElementById('descuento');
 
-    // Agregar un evento de cambio al checkbox
+    
 function total(){
 
     let total = `".$precio_total - intval(substr($_SESSION['Puntos'], 0, -2))."`
