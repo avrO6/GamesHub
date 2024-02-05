0f1ff2382a708fbf00e7968751e7656c26fecf6d
Sergioseves <92640821+Sergioseves@users.noreply.github.com>
2024-02-05 18:45:49 +0100

    Update carrito.php

--- GamesHub/public/carrito.php
+++ GamesHub/public/carrito.php
@@ -14,7 +14,11 @@
 
     if(isset($_POST["eliminar"])){
         unset($_SESSION["Carrito"][$_POST['eliminar']]);
+<<<<<<< Updated upstream
          header("location:./carrito.php");  
+=======
+        header("location:carrito.php");
+>>>>>>> Stashed changes
     }
     $precio_total = 0
 
